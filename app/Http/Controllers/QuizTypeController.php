5de6dd51290d0ad14133c24c78c2fbd4e05a60fb
Layndo Sfr <layndoaqsa17@gmail.com>
2019-07-03 23:13:51 +0700

    fix datatable, change action icon

--- app/Http/Controllers/QuizTypeController.php
+++ app/Http/Controllers/QuizTypeController.php
@@ -14,7 +14,13 @@ class QuizTypeController extends Controller
   public function getData()
   {
     $data = QuizType::orderBy('name')->get();
-    return datatables()->of($data)->make(true);
+    return datatables()->of($data)->addColumn('action', function($row){
+      $btn = '<a href="'.route('quiztype.edit',$row->id).'" class="btn border-info btn-xs text-info-600 btn-flat btn-icon"><i class="icon-pencil6"></i></a>';
+      $btn = $btn.'  <a href="'.route('quiztype.destroy',$row->id).'" class="btn border-warning btn-xs text-warning-600 btn-flat btn-icon"><i class="icon-trash"></i></a>';
+      return $btn;
+    })
+    ->rawColumns(['action'])
+    ->make(true);
   }
   /**
    * Display a listing of the resource.
@@ -79,7 +85,6 @@ class QuizTypeController extends Controller
    */
   public function show($id)
   {
-
   }
 
   /**
@@ -90,7 +95,8 @@ class QuizTypeController extends Controller
    */
   public function edit($id)
   {
-
+    $data = QuizType::find($id);
+    return view('quiz-type.edit', compact('data'));
   }
 
   /**
@@ -99,9 +105,33 @@ class QuizTypeController extends Controller
    * @param  int  $id
    * @return Response
    */
-  public function update($id)
+  public function update(Request $request, $id)
   {
+    // dd($id);
+    $data= QuizType::find($id);
+    $this->validate(request(),
+      [
+        'name' => 'required|max:20|unique:quiz_types,name,'.$data->id.',id',
+        'description' => 'required|max:191',
+      ]
+    );
+    if(!empty($request->picture)){
+         $file = $request->file('picture');
+         $extension = strtolower($file->getClientOriginalExtension());
+         $filename = $request->name . '.' . $extension;
+         Storage::put('images/' . $filename, File::get($file));
+         $file_server = Storage::get('images/' . $filename);
+         $img = Image::make($file_server)->resize(141, 141);
+         $img->save(base_path('public/img/quiztype/' . $filename));
+    }else{
+         $filename=$data->pic_url;
+    }
 
+    $data->name=$request->name;
+    $data->description=$request->description;
+    $data->pic_url=$filename;
+    $data->save();
+    return redirect()->route('quiztype.index');
   }
 
   /**
--- resources/views/quiz-type/edit.blade.php
+++ resources/views/quiz-type/edit.blade.php
@@ -13,7 +13,7 @@
             <li><a href="{{url('admin/dashboard')}}"><i class="icon-home2 position-left"></i> Home</a></li>
             <li><a href="">Master Data</a></li>
             <li><a href="{{route('quiztype.index')}}">Quiz Type</a></li>
-            <li class="active">Create</li>
+            <li class="active">Edit</li>
         </ul>
     </div>
 </div>
@@ -32,15 +32,15 @@
 			{{-- DataTables has the option of being able to <code>save the state</code> of a table: its paging position, ordering state etc., so that is can be restored when the user reloads a page, or comes back to the page after visiting a sub-page. This state saving ability is enabled by the <code>stateSave</code> option. The <code>duration</code> for which the saved state is valid can be set using the <code>stateDuration</code> initialisation parameter (2 hours by default). --}}
 		{{-- </div> --}}
     <div class="panel-body">
-  		<form class="form-horizontal form-validate-jquery" action="{{route('quiztype.store')}}" method="post" enctype="multipart/form-data" files=true>
-        {{ csrf_field() }}
+  		<form class="form-horizontal form-validate-jquery" action="{{route('quiztype.update',$data->id)}}" method="post" enctype="multipart/form-data" files=true>
+        @method('PUT')
+        @csrf
   			<fieldset class="content-group">
   				<legend class="text-bold">Creat Quiz Type</legend>
-
           <div class="form-group">
             <label class="control-label col-lg-3">Type Name <span class="text-danger">*</span></label>
             <div class="col-lg-9">
-              <input type="text" name="name" class="form-control" value="{{ old('name') }}" placeholder="">
+              <input type="text" class="form-control" placeholder="" name="name" value="{{old('name') ? old('name') : $data->name}}">
                 @if ($errors->has('name'))
                 <label style="padding-top:7px;color:#F44336;">
                     <strong><i class="fa fa-times-circle"></i> {{ $errors->first('name') }}</strong>
@@ -51,7 +51,7 @@
           <div class="form-group">
             <label class="control-label col-lg-3">Description <span class="text-danger">*</span></label>
             <div class="col-lg-9">
-              <textarea type="text" name="description" rows="3" class="form-control"  placeholder="">{{ old('description') }}</textarea>
+              <textarea type="text" name="description" rows="3" class="form-control"  placeholder="">{{old('description') ? old('description') : $data->description}}</textarea>
                 @if ($errors->has('description'))
                 <label style="padding-top:7px;color:#F44336;">
                     <strong><i class="fa fa-times-circle"></i>{{ $errors->first('description') }}</strong>
@@ -62,6 +62,7 @@
           <div class="form-group">
   					<label class="control-label col-lg-3">Picture</label>
   					<div class="col-lg-9">
+              <img class="img-responsive" src="{{asset('img/quiztype/'.$data->pic_url.'')}}" alt="Avatar" title="Change the avatar">
   						<input type="file" name="picture" class="form-control">
   					</div>
   				</div>
