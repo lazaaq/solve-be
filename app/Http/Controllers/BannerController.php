36631748f710f59a73dcff85919c50e264f9ee4e
Layndo Sfr <layndoaqsa17@gmail.com>
2019-07-18 15:14:49 +0700

    banner crud

--- app/Http/Controllers/BannerController.php
+++ app/Http/Controllers/BannerController.php
@@ -3,8 +3,169 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Facades\Input;
+use Intervention\Image\Facades\Image;
+use File;
+use App\Banner;
 
 class BannerController extends Controller
 {
-    //
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function getData()
+    {
+     $data = Banner::all();
+     return datatables()->of($data)
+        ->addColumn('action', function($row){
+            // $btn = '<a href="'.route('banner.show',$row->id).'" title="View" class="btn border-success btn-xs text-success-600 btn-flat btn-icon"><i class="glyphicon glyphicon-eye-open"></i></a>';
+            $btn = '<a href="'.route('banner.edit',$row->id).'" title="Edit" class="btn border-info btn-xs text-info-600 btn-flat btn-icon"><i class="icon-pencil6"></i></a>';
+            $btn = $btn.'  <button id="delete" class="btn border-warning btn-xs text-warning-600 btn-flat btn-icon"><i class="icon-trash"></i></button>';
+            // $btn = $btn.'  <a href="'.route('banner.destroy',$row->id).'" title="Delete" class="btn border-warning btn-xs text-warning-600 btn-flat btn-icon"><i class="icon-trash"></i></a>';
+            return $btn;
+        })
+        ->addColumn('isViewed', function($row){
+            // if ($row->isView = 0) {
+            //   $row->isView = "ON";
+            // }else {
+            //   $row->$isView = "OFF";
+            // }
+            return $row->isView;
+        })
+        ->addColumn('pictures', function($row){
+          $row->picture = route('banner.picture',$row->id);
+          $picture = '<img class="img-responsive" src="'.route('banner.picture',$row->id).'" alt="Banner" title="Banner" width="100%">';
+          return $picture;
+        })
+        ->rawColumns(['action','pictures','isViewed'])
+        ->make(true);
+    }
+    public function index()
+    {
+        return view('banner.index');
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('banner.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+      $this->validate(request(),
+        [
+          'description' => 'required|max:191',
+          'picture' => 'required|max:2048|mimes:png,jpg,jpeg',
+          'link_to' => 'required|max:191',
+          'is_view' => 'required',
+        ]
+      );
+      if(!empty($request->picture)){
+           $file = $request->file('picture');
+           $extension = strtolower($file->getClientOriginalExtension());
+           $filename = uniqid() . '.' . $extension;
+           Storage::put('public/images/banner/' . $filename, File::get($file));
+         }else{
+           $filename='';
+         }
+         // dd($filename);
+      Banner::create(
+        [
+              'description'=>request('description'),
+              'picture'=>$filename,
+              'linkTo' => request('link_to'),
+              'isView' => request('is_view'),
+        ]
+      );
+      return redirect(route('banner.index'));
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        $data=Banner::find($id);
+        return view('banner.edit', compact('data'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+     public function update(Request $request, $id)
+     {
+       $data= Banner::find($id);
+       $this->validate(request(),
+         [
+           'description' => 'required|max:191',
+           'picture' => 'max:2048|mimes:png,jpg,jpeg',
+           'link_to' => 'required|max:191',
+           'is_view' => 'required',
+         ]
+       );
+       if(!empty($request->picture)){
+            $file = $request->file('picture');
+            $extension = strtolower($file->getClientOriginalExtension());
+            $filename = uniqid() . '.' . $extension;
+            Storage::delete('public/images/banner/' . $data->picture);
+            Storage::put('public/images/banner/' . $filename, File::get($file));
+       }else{
+            $filename=$data->picture;
+       }
+       $data->description=$request->description;
+       $data->linkTo=$request->link_to;
+       $data->isView=$request->is_view;
+       $data->picture=$filename;
+       $data->save();
+       return redirect()->route('banner.index');
+     }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+
+    public function picture($id)
+    {
+      $data = Banner::find($id);
+      return Image::make(Storage::get('public/images/banner/'.$data->picture))->response();
+    }
 }
---
+++ resources/views/banner/index.blade.php
@@ -0,0 +1,110 @@
+@extends('layouts.app')
+@section('content')
+<!-- Page header -->
+<div class="page-header">
+    <div class="page-header-content">
+        <div class="page-title">
+            <h4><i class=""></i> <span class="text-semibold">Banners</span></h4>
+        </div>
+    </div>
+
+    <div class="breadcrumb-line breadcrumb-line-component">
+        <ul class="breadcrumb">
+            <li><a href="{{url('admin/dashboard')}}"><i class="icon-home2 position-left"></i> Home</a></li>
+            <li><a href="">Master Data</a></li>
+            <li class="active">Banners</li>
+        </ul>
+    </div>
+</div>
+<!-- /page header -->
+
+
+<!-- Content area -->
+<div class="content">
+  <!-- State saving -->
+	<div class="panel panel-flat">
+		{{-- <div class="panel-heading">
+			<h5 class="panel-title">State saving</h5>
+		</div> --}}
+
+		{{-- <div class="panel-body"> --}}
+			{{-- DataTables has the option of being able to <code>save the state</code> of a table: its paging position, ordering state etc., so that is can be restored when the user reloads a page, or comes back to the page after visiting a sub-page. This state saving ability is enabled by the <code>stateSave</code> option. The <code>duration</code> for which the saved state is valid can be set using the <code>stateDuration</code> initialisation parameter (2 hours by default). --}}
+		{{-- </div> --}}
+    <div style="padding:20px">
+      <a href="{{route('banner.create')}}" class="btn btn-primary btn-sm bg-primary-800"><i class="icon-add position-left"></i>Create New</a>
+    	<table class="table" id="table-banner" class="display" style="width:100%">
+  			<thead>
+      		<tr>
+             <th>Id</th>
+             <th>Picture</th>
+             <th>Linked To</th>
+             <th>Description</th>
+             <th class="col-md-1">Viewed</th>
+             <th class="col-md-2">Action</th>
+          </tr>
+  			</thead>
+  			<tbody>
+  			</tbody>
+  		</table>
+    </div>
+	</div>
+	<!-- /state saving -->
+</div>
+<!-- /content area -->
+@endsection
+@push('after_script')
+  <script>
+  var tableQuizType;
+    $(document).ready(function(){
+  		/* tabel user */
+      tableQuizType = $('#table-banner').DataTable({
+        processing	: true,
+        language: {
+                    search: "_INPUT_",
+                    searchPlaceholder: "Search records"
+                  },
+        // dom 		: "<fl<t>ip>",
+  			serverSide	: true,
+  			stateSave: true,
+        ajax		: {
+            url: "{{ url('table/data-banner') }}",
+            type: "GET",
+        },
+        columns: [
+            { data: 'id', name:'id', visible:false},
+            { data: 'pictures', name:'pictures', visible:true},
+            { data: 'linkTo', name:'linkTo', visible:true},
+            { data: 'description', name:'description', visible:true},
+            { data: 'isViewed', name:'isViewed', visible:true},
+            { data: 'action', name:'action', visible:true},
+        ],
+      });
+      $('#table-banner tbody').on( 'click', 'button', function () {
+        var data = tableQuizType.row( $(this).parents('tr') ).data();
+        swal({
+          // title: "Are you sure?",
+          text: "Are you sure to delete data?",
+          icon: "warning",
+          buttons: true,
+          dangerMode: true,
+        })
+        .then((willDelete) => {
+          if (willDelete) {
+            $.ajax({
+              url: "{{ url('admin/banner/delete') }}"+"/"+data['id'],
+              method: 'get',
+              success: function(result){
+                tableQuizType.ajax.reload();
+                swal("Poof! Your imaginary file has been deleted!", {
+                  icon: "success",
+                });
+              }
+            });
+          } else {
+            swal("Your imaginary file is safe!");
+          }
+        });
+      });
+    });
+  </script>
+@endpush
