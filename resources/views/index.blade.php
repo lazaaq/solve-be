44e6e9fa86cf12ba903d435746dc50d8c53d2b81
Layndo Sfr <layndoaqsa17@gmail.com>
2019-07-22 13:50:52 +0700

    calon tampilan dashboar

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -21,9 +21,135 @@
 
 <!-- Content area -->
 <div class="content">
+  <div class="row">
+    <div class="col-md-6">
+      <!-- Simple list -->
+    		<div class="panel panel-flat">
+    			<div class="panel-heading">
+    				<h5 class="panel-title">Simple user list</h5>
+    				<div class="heading-elements">
+    					<ul class="icons-list">
+            		<li><a data-action="collapse"></a></li>
+            		<li><a data-action="reload"></a></li>
+            		<li><a data-action="close"></a></li>
+            	</ul>
+          	</div>
+    			</div>
+
+    			<div class="panel-body">
+    				<ul class="media-list">
+    					<li class="media-header">The Big Four</li>
+
+    					<li class="media">
+    						<div class="media-left media-middle">
+    							<a href="#">
+    								<img src="{{asset('img/avatar.png')}}" class="img-circle" alt="">
+    							</a>
+    						</div>
+
+    						<div class="media-body">
+    							<div class="media-heading text-semibold">James Alexander</div>
+    							<span class="text-muted">Development</span>
+    						</div>
+
+    						<div class="media-right media-middle">
+    							<ul class="icons-list icons-list-extended text-nowrap">
+                  	<li style="font-size:18px"><b>9478</b></li>
+                  	<li><i style="font-size:25px" class="icon-trophy2"></i></li>
+                	</ul>
+    						</div>
+    					</li>
+
+    					<li class="media">
+    						<div class="media-left media-middle">
+    							<a href="#">
+                    <img src="{{asset('img/avatar.png')}}" class="img-circle" alt="">
+    							</a>
+    						</div>
+
+    						<div class="media-body">
+    							<div class="media-heading text-semibold">Jeremy Victorino</div>
+    							<span class="text-muted">Finances</span>
+    						</div>
+
+    						<div class="media-right media-middle">
+    							<ul class="icons-list icons-list-extended text-nowrap">
+                    <li style="font-size:18px"><b>9478</b></li>
+                  	<li><i style="font-size:25px" class="icon-trophy2"></i></li>
+                	</ul>
+    						</div>
+    					</li>
+
+    					<li class="media">
+    						<div class="media-left media-middle">
+    							<a href="#">
+                    <img src="{{asset('img/avatar.png')}}" class="img-circle" alt="">
+    							</a>
+    						</div>
+
+    						<div class="media-body">
+    							<div class="media-heading text-semibold">Margo Baker</div>
+    							<span class="text-muted">Marketing</span>
+    						</div>
+
+    						<div class="media-right media-middle">
+    							<ul class="icons-list icons-list-extended text-nowrap">
+                    <li style="font-size:18px"><b>9478</b></li>
+                  	<li><i style="font-size:25px" class="icon-trophy2"></i></li>
+                	</ul>
+    						</div>
+    					</li>
+
+    					<li class="media">
+    						<div class="media-left media-middle">
+    							<a href="#">
+                    <img src="{{asset('img/avatar.png')}}" class="img-circle" alt="">
+    							</a>
+    						</div>
+
+    						<div class="media-body">
+    							<div class="media-heading text-semibold">Monica Smith</div>
+    							<span class="text-muted">Design</span>
+    						</div>
+
+    						<div class="media-right media-middle">
+    							<ul class="icons-list icons-list-extended text-nowrap">
+                    <li style="font-size:18px"><b>9478</b></li>
+                  	<li><i style="font-size:25px" class="icon-trophy2"></i></li>
+                	</ul>
+    						</div>
+    					</li>
+
+    					<li class="media-header">-----------------------------------------</li>
+
+    					<li class="media">
+    						<div class="media-left media-middle">
+    							<a href="#">
+                    <img src="{{asset('img/avatar.png')}}" class="img-circle" alt="">
+    							</a>
+    						</div>
+
+    						<div class="media-body">
+    							<div class="media-heading text-semibold">Bastian Miller</div>
+    							<span class="text-muted">Web dev</span>
+    						</div>
+
+    						<div class="media-right media-middle">
+    							<ul class="icons-list icons-list-extended text-nowrap">
+                    <li style="font-size:18px"><b>9478</b></li>
+                  	<li><i style="font-size:25px" class="icon-trophy2"></i></li>
+                	</ul>
+    						</div>
+    					</li>
+    				</ul>
+    			</div>
+    		</div>
+    		<!-- /simple list -->
+    </div>
+  </div>
 
     <!-- Simple panel -->
-    <div class="panel panel-flat">
+    {{-- <div class="panel panel-flat">
         <div class="panel-heading">
             <h5 class="panel-title">Simple panel</h5>
         </div>
@@ -38,7 +164,7 @@
             <h6 class="text-semibold">How does it work?</h6>
             <p>You open one of the starter pages, add necessary plugins, uncomment paths to files in components.less file, compile new CSS. That's it. I'd also recommend to open one of main pages with functionality you need and copy all paths/JS code from there to your new page, it's just faster and easier.</p>
         </div>
-    </div>
+    </div> --}}
     <!-- /simple panel -->
 
 </div>
