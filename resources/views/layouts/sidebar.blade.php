b261edcf78b6e09b5839079203a79be7221509b7
Dwi Noviandaru <dnoviandaru@gmail.com>
2019-08-02 16:22:28 +0700

    delete sidebar starter kit

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -5,12 +5,6 @@
             <!-- Main -->
             <li class="navigation-header"><span>Main</span> <i class="icon-menu" title="Main pages"></i></li>
             <li><a href="{{url('admin/dashboard')}}"><i class="icon-home4"></i> <span>Dashboard</span></a></li>
-            <li>
-                <a href="#"><i class="icon-stack"></i> <span>Starter kit</span></a>
-                <ul>
-                    <li><a href="#">Horizontal navigation</a></li>
-                </ul>
-            </li>
             <li class="navigation-header"><span>Master Data</span> <i class="icon-menu" title="Main pages"></i></li>
             <li><a href="{{route('user.index')}}"><i class="icon-user"></i> <span>User</span></a></li>
             <li><a href="{{route('quizcategory.index')}}"><i class="icon-home4"></i> <span>Category</span></a></li>
