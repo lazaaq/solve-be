55a43eb9748a640f45df8a9f74b712e5d22f4aee
Layndo Sfr <layndoaqsa17@gmail.com>
2019-08-29 17:30:21 +0700

    reset password:

---
+++ resources/views/auth/passwords/success.blade.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>RUKO - Rumah Korea</title>
+
+	<!-- Global stylesheets -->
+	<link href="https://fonts.googleapis.com/css?family=Roboto:400,300,100,500,700,900" rel="stylesheet" type="text/css">
+	<link href="{{asset('css/icons/icomoon/styles.css')}}" rel="stylesheet" type="text/css">
+	<link href="{{asset('css/bootstrap.css')}}" rel="stylesheet" type="text/css">
+	<link href="{{asset('css/core.css')}}" rel="stylesheet" type="text/css">
+	<link href="{{asset('css/components.css')}}" rel="stylesheet" type="text/css">
+	<link href="{{asset('css/colors.css')}}" rel="stylesheet" type="text/css">
+	<!-- /global stylesheets -->
+
+	<!-- Core JS files -->
+	<script type="text/javascript" src="{{asset('js/libraries/jquery.min.js')}}"></script>
+	<script type="text/javascript" src="{{asset('js/libraries/bootstrap.min.js')}}"></script>
+	<!-- /core JS files -->
+
+	<!-- Theme JS files -->
+	<script type="text/javascript" src="{{asset('js/app.js')}}"></script>
+	<!-- /theme JS files -->
+
+</head>
+
+<body class="login-container">
+
+	<!-- Page container -->
+	<div class="page-container">
+
+		<!-- Page content -->
+		<div class="page-content">
+
+			<!-- Main content -->
+			<div class="content-wrapper">
+
+				<!-- Content area -->
+				<div class="content">
+
+					<!-- Simple login form -->
+            <div class="panel panel-body login-form">
+							<div class="text-center">
+								<div class="icon-object border-slate-300 text-slate-300"><i class="icon-reading"></i></div>
+								<h5 class="content-group">Change Password Success <small class="display-block">Please back to Ruko Apps.</small></h5>
+							</div>
+						</div>
+					<!-- /simple login form -->
+
+				</div>
+				<!-- /content area -->
+
+			</div>
+			<!-- /main content -->
+
+		</div>
+		<!-- /page content -->
+
+	</div>
+	<!-- /page container -->
+
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -21,60 +21,64 @@ Auth::routes();
 // Auth::routes();
 
     Route::get('/', 'Auth\LoginController@showLoginForm');
-
+    Route:: get ('/coba', function () {
+       return view('layout.layoutclean');
+    })->middleware('isAdmin')->middleware('verified');;
    //admin
    Route::group(['middleware' => 'auth'], function () {
-   Route::group(['middleware' => ['role:admin'],'prefix' => '/admin'], function () {
-     Route::resource('dashboard', 'DashboardController');
-     Route::resource('question', 'QuestionController');
-     Route::resource('user', 'UserController')->except('destroy');
-     Route::resource('answersave', 'AnswerSaveController');
-     Route::resource('quiztype', 'QuizTypeController')->except('destroy');
-     Route::resource('quizcategory', 'QuizCategoryController')->except('destroy');
-     Route::resource('lecture', 'LectureController');
-     Route::resource('collager', 'CollagerController');
-     Route::resource('quizcollager', 'QuizCollagerController');
-     Route::resource('quiz', 'QuizController')->except('destroy');
-     Route::resource('banner', 'BannerController')->except('destroy');
-     Route::resource('version', 'VersionAppController')->except('destroy');
+     Route:: get ('/success-reset', function () {
+        return view('auth.passwords.success');
+     });
+     Route::group(['middleware' => ['role:admin'],'prefix' => '/admin'], function () {
+       Route::resource('dashboard', 'DashboardController');
+       Route::resource('question', 'QuestionController');
+       Route::resource('user', 'UserController')->except('destroy');
+       Route::resource('answersave', 'AnswerSaveController');
+       Route::resource('quiztype', 'QuizTypeController')->except('destroy');
+       Route::resource('quizcategory', 'QuizCategoryController')->except('destroy');
+       Route::resource('lecture', 'LectureController');
+       Route::resource('collager', 'CollagerController');
+       Route::resource('quizcollager', 'QuizCollagerController');
+       Route::resource('quiz', 'QuizController')->except('destroy');
+       Route::resource('banner', 'BannerController')->except('destroy');
+       Route::resource('version', 'VersionAppController')->except('destroy');
 
-     Route::get('quiz/question/{id}','QuestionController@create')->name('quisz.question');
-     Route::get('quiz/question/{id}/add','QuestionController@add')->name('quiz.questionAdd');
-     Route::get('quiz/bulk/{id}/import','QuizController@import')->name('quiz.import');
-     Route::post('quiz/bulk/{id}/import','QuizController@saveImport')->name('quiz.saveImport');
-     Route::get('quiz/import/download', 'QuizController@downloadTemplate')->name('quiz.downloadTemplate');
-     Route::get('quiz/export/{id}', 'QuizController@export')->name('quiz.export');
+       Route::get('quiz/question/{id}','QuestionController@create')->name('quisz.question');
+       Route::get('quiz/question/{id}/add','QuestionController@add')->name('quiz.questionAdd');
+       Route::get('quiz/bulk/{id}/import','QuizController@import')->name('quiz.import');
+       Route::post('quiz/bulk/{id}/import','QuizController@saveImport')->name('quiz.saveImport');
+       Route::get('quiz/import/download', 'QuizController@downloadTemplate')->name('quiz.downloadTemplate');
+       Route::get('quiz/export/{id}', 'QuizController@export')->name('quiz.export');
 
-     Route::get('user/delete/{id}', 'UserController@destroy')->name('user.destroy');
-     Route::put('user/profile/{id}', 'UserController@updateProfil')->name('user.updateProfil');
-     Route::put('user/profile/password/{id}', 'UserController@updatePassword')->name('user.updatePassword');
+       Route::get('user/delete/{id}', 'UserController@destroy')->name('user.destroy');
+       Route::put('user/profile/{id}', 'UserController@updateProfil')->name('user.updateProfil');
+       Route::put('user/profile/password/{id}', 'UserController@updatePassword')->name('user.updatePassword');
 
-     Route::get('quiztype/delete/{id}', 'QuizTypeController@destroy')->name('quiztype.destroy');
-     Route::get('quiz/delete/{id}', 'QuizController@destroy')->name('quiz.destroy');
-     Route::get('question/delete/{id}', 'QuestionController@destroy')->name('question.destroy');
-     Route::get('quizcategory/delete/{id}', 'QuizCategoryController@destroy')->name('quizcategory.destroy');
+       Route::get('quiztype/delete/{id}', 'QuizTypeController@destroy')->name('quiztype.destroy');
+       Route::get('quiz/delete/{id}', 'QuizController@destroy')->name('quiz.destroy');
+       Route::get('question/delete/{id}', 'QuestionController@destroy')->name('question.destroy');
+       Route::get('quizcategory/delete/{id}', 'QuizCategoryController@destroy')->name('quizcategory.destroy');
 
-     Route::get('banner/delete/{id}', 'BannerController@destroy')->name('banner.destroy');
-     Route::post('banner/change-is-view/{id}', 'BannerController@changeIsView')->name('banner.changeIsView');
+       Route::get('banner/delete/{id}', 'BannerController@destroy')->name('banner.destroy');
+       Route::post('banner/change-is-view/{id}', 'BannerController@changeIsView')->name('banner.changeIsView');
 
-     Route::get('version/delete/{id}', 'VersionAppController@destroy')->name('version.destroy');
+       Route::get('version/delete/{id}', 'VersionAppController@destroy')->name('version.destroy');
+     });
+     Route::group(['middleware' => ['role:admin'],'prefix' => '/table'], function () {
+       Route::get('/data-quiz-type', 'QuizTypeController@getData');
+       Route::get('/data-quiz-category', 'QuizCategoryController@getData');
+       Route::get('/data-quiz', 'QuizController@getData');
+       Route::get('/data-user', 'UserController@getData');
+       Route::get('/data-banner', 'BannerController@getData');
+       Route::get('/data-version', 'VersionAppController@getData');
+     });
 
-   });
-   Route::group(['middleware' => ['role:admin'],'prefix' => '/table'], function () {
-     Route::get('/data-quiz-type', 'QuizTypeController@getData');
-     Route::get('/data-quiz-category', 'QuizCategoryController@getData');
-     Route::get('/data-quiz', 'QuizController@getData');
-     Route::get('/data-user', 'UserController@getData');
-     Route::get('/data-banner', 'BannerController@getData');
-     Route::get('/data-version', 'VersionAppController@getData');
-   });
+     Route::group(['middleware' => ['role:admin'],'prefix' => '/select'], function () {
+      Route::get('/data-quiz-category', 'QuizCategoryController@getSelect');
+      Route::get('/data-quiz-category/{id}', 'QuizCategoryController@getPreSelect');
+    });
 
-   Route::group(['middleware' => ['role:admin'],'prefix' => '/select'], function () {
-    Route::get('/data-quiz-category', 'QuizCategoryController@getSelect');
-    Route::get('/data-quiz-category/{id}', 'QuizCategoryController@getPreSelect');
   });
-
-});
 Route::group(['prefix' => '/storage'], function () {
   Route::get('user/{id}', 'UserController@picture')->name('user.picture');
   Route::get('quiz_type/{id}', 'QuizTypeController@picture')->name('quiztype.picture');
