fe0366517fa9114169bf4a2769ed677e36de4f7e
Layndo Sfr <layndoaqsa17@gmail.com>
2019-09-11 19:11:29 +0700

    nyoba leaderboard dashboard, tak komen, meh bener, madang sek

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -31,6 +31,36 @@ class DashboardController extends Controller
       $totalGamePlayedBefore  = QuizCollager::whereBetween('created_at',[Carbon::today()->addDay(-1),Carbon::today()])->count();
       $quiz = Quiz::all()->sortBy('quiz_type_id');
       $totalQuiz = $quiz->count();
+
+      // $score = QuizCollager::leftJoin('collagers', 'quiz_collagers.collager_id', 'collagers.id')
+      //                       ->leftJoin('users', 'collagers.user_id', 'users.id')
+      //                       ->groupBy('quiz_collagers.collager_id','users.username', 'users.name','users.id','users.picture', 'quiz_collagers.quiz_id')
+      //                       // ->where('quiz_collagers.quiz_id', 2)
+      //                       ->selectRaw('users.id as user_id, quiz_collagers.collager_id, users.username, users.picture, max(quiz_collagers.total_score) as total_score, users.name, quiz_collagers.quiz_id')
+      //                       // ->limit(5)
+      //                       ->get();
+      //                       // dd($score);
+      // $collection = [];
+      // foreach ($quiz as $i => $quizs) {
+      //   $collection[$i] = [
+      //     'quiz_id'     => $quizs['id'],
+      //     'quiz_title'  => $quizs['title'],
+      //     'type'        => $quizs->quizType['name'],
+      //     'category'    => $quizs->quizType->QuizCategory['name'],
+      //     'leaderboard' => [],
+      //   ];
+      //   foreach ($score as $j => $scores) {
+      //     if ($scores->quiz_id == $quizs['id']) {
+      //       $leaderboard = $scores->orderBy('total_score','DESC')->limit(5)->get();
+      //       $collection[$i]['leaderboard'] = $leaderboard;
+      //       // $collection[$i]['leaderboard'][0]['name']  = $scores->name;
+      //       // $collection[$i]['leaderboard'][0]['total_score']  = $scores->total_score;
+      //       // $collection[$i]['leaderboard'][0]['pictures']  = $scores->pictures;
+      //     }
+      //   }
+      // }
+      // dd($collection);
+
       return view('index', compact ('quiz','memberOnline','totalMember','totalQuiz','totalQuizType','totalGamePlayed','totalGamePlayedBefore'));
     }
 
