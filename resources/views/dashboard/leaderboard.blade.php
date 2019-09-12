638453419bae965f42b7024c5f70fc13a0523a40
Layndo Sfr <layndoaqsa17@gmail.com>
2019-09-12 09:58:11 +0700

    dashboard

--- resources/views/dashboard/leaderboard.blade.php
+++ resources/views/dashboard/leaderboard.blade.php
@@ -1,9 +1,9 @@
-@foreach ($quiz as $key => $value)
+@foreach ($collection as $key => $value)
 <div class="col-md-4">
   <!-- Simple list -->
-    <div class="panel panel-flat">
+    <div class="panel panel-flat" style="height:400px;overflow:scroll;">
       <div class="panel-heading">
-        <h5 class="panel-title">{{$value->title}}</h5>
+        <h5 class="panel-title">{{$value['title']}}</h5>
         <div class="heading-elements">
           <ul class="icons-list">
             <li><a data-action="collapse"></a></li>
@@ -15,35 +15,41 @@
 
       <div class="panel-body">
         <ul class="media-list">
-          <li class="media-header">Quiz Type : {{$value->quizType['name']}}</li>
-          @foreach ($value->quizCollager->sortByDesc('total_score')->take(5) as $key2 => $value2)
-          <li class="media">
-            <div class="media-left media-middle">
-              <a href="#">
-                @if($value2->collager->user['picture'] == 'avatar.png')
-                  <img src="{{asset('img/avatar.png')}}" class="img-circle" alt="">
-                @else
-                  <img src="{{route('user.picture',$value2->collager->user['id'])}}" class="img-circle" alt="">
-                @endif
-              </a>
-            </div>
+          <li class="media-header">Quiz Type : {{$value['type']}}</li>
+          @if (empty($value['leaderboard']))
+            <li class="media">
+              <div class="media-body">
+                <div class="media-heading text-semibold">Empty Data</div>
+                {{-- <span class="text-muted">{{$value2->username}}</span> --}}
+              </div>
+            </li>
+          @else
+            @foreach ($value['leaderboard'] as $key2 => $value2)
+              <li class="media">
+                <div class="media-left media-middle">
+                  <a href="#">
+                    @if($value2->picture == 'avatar.png' || empty($value2->picture))
+                      <img src="{{asset('img/avatar.png')}}" class="img-circle" alt="avatar">
+                    @else
+                      <img src="{{route('user.picture',$value2->user_id)}}" class="img-circle" alt="avatar">
+                    @endif
+                  </a>
+                </div>
 
-            <div class="media-body">
-              <div class="media-heading text-semibold">{{$value2->collager->user['name']}}</div>
-              <span class="text-muted">{{$value2->collager->user['username']}}</span>
-            </div>
+                <div class="media-body">
+                  <div class="media-heading text-semibold">{{$value2->name}}</div>
+                  <span class="text-muted">{{$value2->username}}</span>
+                </div>
 
-            <div class="media-right media-middle">
-              <ul class="icons-list icons-list-extended text-nowrap">
-                <li style="font-size:18px"><b>{{$value2->total_score}}</b></li>
-                <li><i style="font-size:25px" class="icon-trophy2"></i></li>
-              </ul>
-            </div>
-          </li>
-          {{-- @if($key2 == 4)
-              @break
-          @endif --}}
-          @endforeach
+                <div class="media-right media-middle">
+                  <ul class="icons-list icons-list-extended text-nowrap">
+                    <li style="font-size:18px"><b>{{$value2->total_score}}</b></li>
+                    <li><i style="font-size:25px" class="icon-trophy2"></i></li>
+                  </ul>
+                </div>
+              </li>
+            @endforeach
+          @endif
         </ul>
       </div>
     </div>
