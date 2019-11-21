aa62f49db63a27fd12b351b8c6785cb9cc80a67b
Dwi Noviandaru <dnoviandaru@gmail.com>
2019-11-21 13:56:17 +0700

    edit migration table question

--- database/migrations/2019_07_02_074800_create_questions_table.php
+++ database/migrations/2019_07_02_074800_create_questions_table.php
@@ -10,7 +10,7 @@ class CreateQuestionsTable extends Migration {
 		Schema::create('questions', function(Blueprint $table) {
 			$table->increments('id');
 			$table->integer('quiz_id')->unsigned();
-			$table->string('question');
+			$table->string('question',255);
 			$table->string('pic_url')->nullable();
 			$table->timestamps();
 			$table->softDeletes();
