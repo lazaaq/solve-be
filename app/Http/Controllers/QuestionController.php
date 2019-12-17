de89226fa3e48bd4e8576befb636f7b028a6f918
Dwi Noviandaru <dnoviandaru@gmail.com>
2019-12-17 14:51:50 +0700

    edit api

--- app/Http/Controllers/QuestionController.php
+++ app/Http/Controllers/QuestionController.php
@@ -310,7 +310,7 @@ class QuestionController extends Controller
       $collection = [];
       foreach ($question as $i => $item) {
         $opt = $item->answer()->orderBy('option', 'asc')->get();
-        $opt->chosen = 0;
+        $opt['chosen'] = 0;
         $collection[$i] = [
           'id' => $item['id'],
           'question' => $item['question'],
