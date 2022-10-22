e404979f76958a9e931cc70065c920bbcc985dee
lazaaq <lana.saiful.aqil@mail.ugm.ac.id>
2022-10-22 21:28:25 +0700

    add static image for each types, add accessor to it

--- database/seeds/QuizTypesTableSeeder.php
+++ database/seeds/QuizTypesTableSeeder.php
@@ -14,14 +14,14 @@ class QuizTypesTableSeeder extends Seeder
     public function run()
     {
         $categories = array('Astronomi', 'Biologi', 'Ekonomi', 'Fisika', 'Geografi', 'Kebumian', 'Kimia', 'Komputer', 'Matematika');
-        $types = array('Kota/Kabupaten', 'Provinsi', 'Nasional');
+        $types = array('Kota-Kabupaten', 'Provinsi', 'Nasional');
         for($i=0; $i<count($categories); $i++) {
             for($j=0; $j<count($types); $j++) { // tingkat kota, provinsi, nasional
                 QuizType::create([
                     'quiz_category_id' => ($i+1),
                     'name' => $types[$j] . ' ' . $categories[$i],
                     'description' => $types[$j] . ' ' . $categories[$i],
-                    'pic_url' => 'blank.jpg',
+                    'pic_url' => $types[$j] . '.png',
                     'created_by' => 1,
                     'created_at' => '2019-12-18 11:19:58',
                     'updated_at' => '2019-12-18 11:19:58',
