7f96b84ba2b52c9a8120063418fa05af653d98e3
lazaaq <lana.saiful.aqil@mail.ugm.ac.id>
2022-11-03 10:45:33 +0700

    fix quiz temporary

--- app/Http/Controllers/QuizTemporaryController.php
+++ app/Http/Controllers/QuizTemporaryController.php
@@ -12,19 +12,23 @@ class QuizTemporaryController extends Controller
 {
     public function store(Request $request) {
         try {
-            $start_time = date('Y-m-d h:i:s', time());
+            $quizTemporary = QuizTemporary::where('quiz_id', $request->quiz_id)->where('collager_id', $request->collager_id)->first();
+            if ($quizTemporary) {
+                return responseAPI(200, true, $quizTemporary);
+            } else {
+                $start_time = date('Y-m-d h:i:s', time());
 
-            $end_time = new DateTime($start_time);
-            $end_time->add(new DateInterval('PT' . $request->duration . 'M'));
-            $end_time = $end_time->format('Y-m-d h:i:s');
-            $quiz_temporary = QuizTemporary::create([
-                'quiz_id' => $request->quiz_id,
-                'collager_id' => $request->collager_id,
-                'start_time' => $start_time,
-                'end_time' => $end_time,
-            ]);
-
-            return responseAPI(200, true, $quiz_temporary);
+                $end_time = new DateTime($start_time);
+                $end_time->add(new DateInterval('PT' . $request->duration . 'M'));
+                $end_time = $end_time->format('Y-m-d h:i:s');
+                $newQuizTemporary = QuizTemporary::create([
+                    'quiz_id' => $request->quiz_id,
+                    'collager_id' => $request->collager_id,
+                    'start_time' => $start_time,
+                    'end_time' => $end_time,
+                ]);
+                return responseAPI(200, true, $newQuizTemporary);
+            }
         } catch (Exception $e) {
             return responseAPI(400, false, null, $e);
         }
--- database/seeds/QuizTemporaryTableSeeder.php
+++ database/seeds/QuizTemporaryTableSeeder.php
@@ -12,23 +12,11 @@ class QuizTemporaryTableSeeder extends Seeder
      */
     public function run()
     {
-        QuizTemporary::create([
-            "quiz_id" => 1,
-            "collager_id" => 1,
-            'start_time' => '2019-12-01 00:00:00',
-            'end_time' => '2023-12-31 23:59:00',
-        ]);
-        QuizTemporary::create([
-            "quiz_id" => 2,
-            "collager_id" => 1,
-            'start_time' => '2019-12-01 00:00:00',
-            'end_time' => '2023-12-31 23:59:00',
-        ]);
-        QuizTemporary::create([
-            "quiz_id" => 3,
-            "collager_id" => 1,
-            'start_time' => '2019-12-01 00:00:00',
-            'end_time' => '2023-12-31 23:59:00',
-        ]);
+        // QuizTemporary::create([
+        //     "quiz_id" => 1,
+        //     "collager_id" => 1,
+        //     'start_time' => '2019-12-01 00:00:00',
+        //     'end_time' => '2023-12-31 23:59:00',
+        // ]);
     }
 }
